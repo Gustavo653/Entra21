c3ea723d8dd3a06d266d3b6be4c0364921e0cd31
Gustavo <gustavohs2004@gmail.com>
2021-06-14 11:30:13 -0300

    Aula09PT3 Criada

---
+++ Aula09PT3/Exec.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula09PT3
+{
+    class Exec01
+    {
+        private string nome, raca;
+        private int idade;
+
+        public Exec01(string nome, string raca, int idade)
+        {
+            this.nome = nome;
+            this.raca = raca;
+            this.idade = idade;
+        }
+        public void SetNome(string nome)
+        {
+            this.nome = nome;
+        }
+        public string GetNome()
+        {
+            return nome;
+        }
+        public void SetRaca(string raca)
+        {
+            this.raca = raca;
+        }
+        public string GetRaca()
+        {
+            return raca;
+        }
+        public void SetIdade(int idade)
+        {
+            this.idade = idade;
+        }
+        public int GetIdade()
+        {
+            return idade;
+        }
+    }
+    class Exec04
+    {
+        private string nome, cpf;
+        private string[] consultas;
+
+        public Exec04(string nome, string cpf, string[] consultas)
+        {
+            this.nome = nome;
+            this.cpf = cpf;
+            this.consultas = consultas;
+        }
+        public void SetNome(string nome)
+        {
+            this.nome = nome;
+        }
+        public string GetNome()
+        {
+            return nome;
+        }
+        public void SetCpf(string cpf)
+        {
+            this.cpf = cpf;
+        }
+        public string GetCpf()
+        {
+            return cpf;
+        }
+        public void SetConsulta(string[] consultas)
+        {
+            this.consultas = consultas;
+        }
+        public string[] GetConsultas()
+        {
+            return consultas;
+        }
+
+    }
+}
---
+++ Aula09PT3/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Aula09PT3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////01
+            //Console.WriteLine("Preencha um cachorro");
+            //string nome, raca;
+            //int idade;
+            //nome = Console.ReadLine();
+            //raca = Console.ReadLine();
+            //idade = int.Parse(Console.ReadLine());
+            //Exec01 cachorro = new Exec01(nome, raca, idade);
+            //Console.WriteLine(cachorro.GetNome() + cachorro.GetRaca() + cachorro.GetIdade());
+
+            ////EXTRA
+            //Console.WriteLine("Insira outro cachorro");
+            //nome = Console.ReadLine();
+            //raca = Console.ReadLine();
+            //idade = int.Parse(Console.ReadLine());
+            //cachorro.SetNome(nome);
+            //cachorro.SetRaca(raca);
+            //cachorro.SetIdade(idade);
+            //Console.WriteLine(cachorro.GetNome() + cachorro.GetRaca() + cachorro.GetIdade());
+
+            //04
+            Exec04 pessoa = new Exec04("", "", new string[1]);
+            string nome, cpf;
+            string[] consultas = new string[10];
+            Console.WriteLine("Insira um cliente");
+            nome = Console.ReadLine();
+            cpf = Console.ReadLine();
+            Console.WriteLine("Insira as consultas");
+            Console.WriteLine("Para encerrar pressione f");
+            int indice = 0;
+            while (true)
+            {
+
+                Console.WriteLine("Insira a consulta " + (indice + 1));
+                string tempConsulta = Console.ReadLine();
+                if (tempConsulta == "f")
+                {
+                    break;
+                }
+                try
+                {
+                    consultas[indice] = tempConsulta;
+                    indice++;
+
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Chegamos ao limite de 10 consultas");
+                    break;
+                }
+            }
+            pessoa.SetNome(nome);
+            pessoa.SetCpf(cpf);
+            pessoa.SetConsulta(consultas);
+            Console.Clear();
+            Console.WriteLine(pessoa.GetNome());
+            Console.WriteLine(pessoa.GetCpf());
+            for (int i = 0; i < pessoa.GetConsultas().Length; i++)
+            {
+                if(pessoa.GetConsultas()[i] == null || pessoa.GetConsultas()[i] == "")
+                {
+                    break;
+                }
+                Console.WriteLine(pessoa.GetConsultas()[i]);
+            }
+        }
+    }
+}
