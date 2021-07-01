e4518644b28a0f46a43b570e63de58f1a18ce01e
Gustavo <gustavohs2004@gmail.com>
2021-07-01 08:59:09 -0300

    Att Aula09PT6

---
+++ Aula09PT6 - Polimorfismo/Exec03.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula09PT6___Polimorfismo
+{
+    //03
+    class Cliente
+    {
+        public string Nome { get; set; }
+        public double Dinheiro { get; set; }
+        public Cliente(string nome, double dinheiro)
+        {
+            Nome = nome;
+            Dinheiro = dinheiro;
+        }
+    }
+    class Imovel
+    {
+        public string Endereco { get; set; }
+        public double Preco { get; set; }
+
+        public Imovel(string endereco, double preco)
+        {
+            Endereco = endereco;
+            Preco = preco;
+        }
+        public virtual void CalculaValor()
+        {
+            Console.WriteLine("---------------------------");
+            Console.WriteLine("Endereço: " + Endereco);
+            Console.WriteLine("Preço: " + Preco);
+        }
+    }
+    class Novo : Imovel
+    {
+        public double PrecoAdicional { get; set; }
+        public Novo(string endereco, double preco) : base(endereco, preco)
+        {
+        }
+        public override void CalculaValor()
+        {
+            base.CalculaValor();
+            Preco += PrecoAdicional;
+            Console.WriteLine("Imóvel novo");
+            Console.WriteLine("Preço adicional: " + PrecoAdicional);
+            Console.WriteLine("Preço total: " + Preco);
+        }
+    }
+    class Velho : Imovel
+    {
+        public double Desconto { get; set; }
+        public Velho(string endereco, double preco) : base(endereco, preco)
+        {
+        }
+        public override void CalculaValor()
+        {
+            base.CalculaValor();
+            Preco -= Desconto;
+            Console.WriteLine("Imóvel velho");
+            Console.WriteLine("Desconto: " + Desconto);
+            Console.WriteLine("Preço total: " + Preco);
+        }
+    }
+}
--- Aula09PT6 - Polimorfismo/Program.cs
+++ Aula09PT6 - Polimorfismo/Program.cs
@@ -121,167 +121,227 @@ namespace Aula09PT6___Polimorfismo
             //    }
             //}
 
-            //02c
-            Assistente ass = new Assistente("NomeAssistente", 2000);
-            Tecnico tec = new Tecnico("NomeTecnico", 3000);
-            Administrativo adm = new Administrativo("NomeAdministrativo", 3000);
+            ////02c
+            //Assistente ass = new Assistente("NomeAssistente", 2000);
+            //Tecnico tec = new Tecnico("NomeTecnico", 3000);
+            //Administrativo adm = new Administrativo("NomeAdministrativo", 3000);
 
-            ass.NumMatricula = 0;
-            tec.NumMatricula = 1;
-            adm.NumMatricula = 2;
+            //ass.NumMatricula = 0;
+            //tec.NumMatricula = 1;
+            //adm.NumMatricula = 2;
 
-            tec.BonusSalarial = 500;
-            adm.Turno = true;
-            adm.AdicionalNoturno = 400;
+            //tec.BonusSalarial = 500;
+            //adm.Turno = true;
+            //adm.AdicionalNoturno = 400;
 
-            List<Assistente> funcs = new List<Assistente>();
-            funcs.Add(adm);
-            funcs.Add(tec);
-            funcs.Add(ass);
+            //List<Assistente> funcs = new List<Assistente>();
+            //funcs.Add(adm);
+            //funcs.Add(tec);
+            //funcs.Add(ass);
 
+            //while (true)
+            //{
+            //    Console.WriteLine("O que você deseja fazer?");
+            //    Console.WriteLine("1 - Alterar");
+            //    Console.WriteLine("2 - Excluir");
+            //    Console.WriteLine("3 - Sair");
+            //    int x = int.Parse(Console.ReadLine());
+            //    if (x == 1)
+            //    {
+            //        Console.WriteLine("Escolha o ID de um funcionário para alterar");
+            //        for (int i = 0; i < funcs.Count; i++)
+            //        {
+            //            Console.WriteLine("Id: " + (i + 1));
+            //            funcs[i].ExibeDados();
+            //        }
+
+            //        Console.WriteLine("---------------------------");
+
+            //        x = int.Parse(Console.ReadLine());
+            //        x--;
+
+            //        if (funcs[x].NumMatricula == 1)
+            //        {
+            //            tec.ExibeDados();
+            //            Console.WriteLine("Qual dado você deseja alterar?");
+            //            Console.WriteLine("1 - Nome");
+            //            Console.WriteLine("2 - Salario");
+            //            Console.WriteLine("3 - NumMatricula");
+            //            Console.WriteLine("4 - Bonus Salarial");
+            //            x = int.Parse(Console.ReadLine());
+            //            if (x == 1)
+            //            {
+            //                string nome = Console.ReadLine();
+            //                tec.Nome = nome;
+            //            }
+            //            else if (x == 2)
+            //            {
+            //                double salario = double.Parse(Console.ReadLine());
+            //                tec.Salario = salario;
+            //            }
+            //            else if (x == 3)
+            //            {
+            //                int matricula = int.Parse(Console.ReadLine());
+            //                tec.NumMatricula = matricula;
+            //            }
+            //            else
+            //            {
+            //                double bonus = double.Parse(Console.ReadLine());
+            //                tec.BonusSalarial = bonus;
+            //            }
+            //        }
+            //        else if (funcs[x].NumMatricula == 2)
+            //        {
+            //            adm.ExibeDados();
+            //            Console.WriteLine("Qual dado você deseja alterar?");
+            //            Console.WriteLine("1 - Nome");
+            //            Console.WriteLine("2 - Salario");
+            //            Console.WriteLine("3 - NumMatricula");
+            //            Console.WriteLine("4 - Adicional turno");
+            //            Console.WriteLine("5 - Turno");
+            //            x = int.Parse(Console.ReadLine());
+            //            if (x == 1)
+            //            {
+            //                string nome = Console.ReadLine();
+            //                adm.Nome = nome;
+            //            }
+            //            else if (x == 2)
+            //            {
+            //                double salario = double.Parse(Console.ReadLine());
+            //                adm.Salario = salario;
+            //            }
+            //            else if (x == 3)
+            //            {
+            //                int matricula = int.Parse(Console.ReadLine());
+            //                adm.NumMatricula = matricula;
+            //            }
+            //            else if (x == 4)
+            //            {
+            //                double adicional = double.Parse(Console.ReadLine());
+            //                adm.AdicionalNoturno = adicional;
+            //            }
+            //            else
+            //            {
+            //                int turno = int.Parse(Console.ReadLine());
+            //                if (turno == 1)
+            //                {
+            //                    adm.Turno = true;
+            //                }
+            //                else
+            //                {
+            //                    adm.Turno = false;
+            //                }
+            //            }
+            //        }
+            //        else
+            //        {
+            //            ass.ExibeDados();
+            //            Console.WriteLine("Qual dado você deseja alterar?");
+            //            Console.WriteLine("1 - Nome");
+            //            Console.WriteLine("2 - Salario");
+            //            Console.WriteLine("3 - NumMatricula");
+            //            x = int.Parse(Console.ReadLine());
+            //            if (x == 1)
+            //            {
+            //                string nome = Console.ReadLine();
+            //                ass.Nome = nome;
+            //            }
+            //            else if (x == 2)
+            //            {
+            //                double salario = double.Parse(Console.ReadLine());
+            //                ass.Salario = salario;
+            //            }
+            //            else if (x == 3)
+            //            {
+            //                int matricula = int.Parse(Console.ReadLine());
+            //                ass.NumMatricula = matricula;
+            //            }
+            //        }
+
+            //    }
+            //    else if (x == 2)
+            //    {
+            //        Console.WriteLine("Escolha o ID de um funcionário para demitir");
+            //        for (int i = 0; i < funcs.Count; i++)
+            //        {
+            //            Console.WriteLine("Id: " + (i + 1));
+            //            funcs[i].ExibeDados();
+            //        }
+            //        x = int.Parse(Console.ReadLine());
+            //        x--;
+            //        try
+            //        {
+            //            funcs.RemoveAt(x);
+            //        }
+            //        catch (Exception)
+            //        {
+            //            Console.WriteLine("Funcionario inválido!");
+            //        }
+            //        Console.WriteLine("Funcionário demitido!");
+            //    }
+            //    else
+            //    {
+            //        break;
+            //    }
+            //    Console.ReadKey();
+            //    Console.Clear();
+            //}
+
+            //03
+            Cliente cli = new Cliente("Nome", 2500);
+
+            Novo imNovo = new Novo("Rua Novo", 1000);
+            Novo imNovo2 = new Novo("Rua Novo", 2000);
+            Velho imVelho = new Velho("Rua Velho", 2000);
+            Velho imVelho2 = new Velho("Rua Velho", 4000);
+
+            imNovo.PrecoAdicional = 1000;
+            imNovo2.PrecoAdicional = 1000;
+            imVelho.Desconto = 1000;
+            imVelho2.Desconto = 1000;
+
+            List<Imovel> imoveis = new List<Imovel>();
+            imoveis.Add(imNovo);
+            imoveis.Add(imNovo2);
+            imoveis.Add(imVelho);
+            imoveis.Add(imVelho2);
             while (true)
             {
-                Console.WriteLine("O que você deseja fazer?");
-                Console.WriteLine("1 - Alterar");
-                Console.WriteLine("2 - Excluir");
-                Console.WriteLine("3 - Sair");
-                int x = int.Parse(Console.ReadLine());
-                if (x == 1)
+                Console.WriteLine("Nome do cliente: " + cli.Nome);
+                Console.WriteLine("Dinheiro do cliente: " + cli.Dinheiro);
+                Console.WriteLine();
+                for (int i = 0; i < imoveis.Count; i++)
                 {
-                    Console.WriteLine("Escolha o ID de um funcionário para alterar");
-                    for (int i = 0; i < funcs.Count; i++)
-                    {
-                        Console.WriteLine("Id: " + (i + 1));
-                        funcs[i].ExibeDados();
-                    }
+                    Console.WriteLine("ID: " + (i + 1));
+                    imoveis[i].CalculaValor();
+                }
 
-                    Console.WriteLine("---------------------------");
-
-                    x = int.Parse(Console.ReadLine());
-                    x--;
-
-                    if (funcs[x].NumMatricula == 1)
+                Console.WriteLine("Insira o ID desejado");
+                int escolha = int.Parse(Console.ReadLine());
+                escolha--;
+                try
+                {
+                    if(imoveis[escolha].Preco > cli.Dinheiro)
                     {
-                        tec.ExibeDados();
-                        Console.WriteLine("Qual dado você deseja alterar?");
-                        Console.WriteLine("1 - Nome");
-                        Console.WriteLine("2 - Salario");
-                        Console.WriteLine("3 - NumMatricula");
-                        Console.WriteLine("4 - Bonus Salarial");
-                        x = int.Parse(Console.ReadLine());
-                        if (x == 1)
-                        {
-                            string nome = Console.ReadLine();
-                            tec.Nome = nome;
-                        }
-                        else if (x == 2)
-                        {
-                            double salario = double.Parse(Console.ReadLine());
-                            tec.Salario = salario;
-                        }
-                        else if (x == 3)
-                        {
-                            int matricula = int.Parse(Console.ReadLine());
-                            tec.NumMatricula = matricula;
-                        }
-                        else
-                        {
-                            double bonus = double.Parse(Console.ReadLine());
-                            tec.BonusSalarial = bonus;
-                        }
-                    }
-                    else if (funcs[x].NumMatricula == 2)
-                    {
-                        adm.ExibeDados();
-                        Console.WriteLine("Qual dado você deseja alterar?");
-                        Console.WriteLine("1 - Nome");
-                        Console.WriteLine("2 - Salario");
-                        Console.WriteLine("3 - NumMatricula");
-                        Console.WriteLine("4 - Adicional turno");
-                        Console.WriteLine("5 - Turno");
-                        x = int.Parse(Console.ReadLine());
-                        if (x == 1)
-                        {
-                            string nome = Console.ReadLine();
-                            adm.Nome = nome;
-                        }
-                        else if (x == 2)
-                        {
-                            double salario = double.Parse(Console.ReadLine());
-                            adm.Salario = salario;
-                        }
-                        else if (x == 3)
-                        {
-                            int matricula = int.Parse(Console.ReadLine());
-                            adm.NumMatricula = matricula;
-                        }
-                        else if (x == 4)
-                        {
-                            double adicional = double.Parse(Console.ReadLine());
-                            adm.AdicionalNoturno = adicional;
-                        }
-                        else
-                        {
-                            int turno = int.Parse(Console.ReadLine());
-                            if (turno == 1)
-                            {
-                                adm.Turno = true;
-                            }
-                            else
-                            {
-                                adm.Turno = false;
-                            }
-                        }
+                        Console.WriteLine("Você vai precisar parcelar este imóvel.");
+                        double parcelamento = imoveis[escolha].Preco;
+                        parcelamento /= 12;
+                        parcelamento *= 1.1674;
+                        Console.WriteLine("Cada parcela será de: " + parcelamento);
+                        parcelamento *= 12;
+                        Console.WriteLine("O valor que você precisa parcelar será de: " + imoveis[escolha].Preco);
+                        Console.WriteLine("O valor total com acréscimo " + parcelamento);
                     }
                     else
                     {
-                        ass.ExibeDados();
-                        Console.WriteLine("Qual dado você deseja alterar?");
-                        Console.WriteLine("1 - Nome");
-                        Console.WriteLine("2 - Salario");
-                        Console.WriteLine("3 - NumMatricula");
-                        x = int.Parse(Console.ReadLine());
-                        if (x == 1)
-                        {
-                            string nome = Console.ReadLine();
-                            ass.Nome = nome;
-                        }
-                        else if (x == 2)
-                        {
-                            double salario = double.Parse(Console.ReadLine());
-                            ass.Salario = salario;
-                        }
-                        else if (x == 3)
-                        {
-                            int matricula = int.Parse(Console.ReadLine());
-                            ass.NumMatricula = matricula;
-                        }
+                        cli.Dinheiro -= imoveis[escolha].Preco;
+                        Console.WriteLine("Agradecemos pela compra!");
+                        Console.WriteLine("Seu saldo restante é de: " + cli.Dinheiro);
                     }
-
                 }
-                else if (x == 2)
+                catch (Exception)
                 {
-                    Console.WriteLine("Escolha o ID de um funcionário para demitir");
-                    for (int i = 0; i < funcs.Count; i++)
-                    {
-                        Console.WriteLine("Id: " + (i + 1));
-                        funcs[i].ExibeDados();
-                    }
-                    x = int.Parse(Console.ReadLine());
-                    x--;
-                    try
-                    {
-                        funcs.RemoveAt(x);
-                    }
-                    catch (Exception)
-                    {
-                        Console.WriteLine("Funcionario inválido!");
-                    }
-                    Console.WriteLine("Funcionário demitido!");
-                }
-                else
-                {
-                    break;
+                    Console.WriteLine("Imóvel inválido!");
                 }
                 Console.ReadKey();
                 Console.Clear();
