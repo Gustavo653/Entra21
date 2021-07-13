49da9c2be3086078fe1d8c4c88aaa362ce62a750
Gustavo <gustavohs2004@gmail.com>
2021-07-13 11:46:00 -0300

    Att Aula09PT7

---
+++ Aula09PT8 - EnumELambda/Exec01.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula09PT8___EnumELambda
+{
+    enum Nomes
+    {
+        João,
+        Pedro,
+        Carlos
+    };
+}
--- Aula09PT8 - EnumELambda/Program.cs
+++ Aula09PT8 - EnumELambda/Program.cs
@@ -6,7 +6,41 @@ namespace Aula09PT8___EnumELambda
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ////1
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    Console.WriteLine("ID: {0}, Nome: {1}",i+1,(Nomes)i);
+            //}
+            //Console.WriteLine("Qual ID você deseja?");
+            //int escolha = int.Parse(Console.ReadLine());
+            //escolha--;
+            //Console.WriteLine("Nome que você escolheu: " + (Nomes)escolha);
+            //Console.WriteLine(Convert.ToInt32((Nomes)escolha));
+
+            //2
+            Func<int, int> eleva = x =>
+            {
+                if (x <= 10)
+                {
+                    return x * x;
+                }
+                else
+                {
+                    return x;
+                }
+            };
+
+            Console.WriteLine("Insira um numero");
+            int numero = int.Parse(Console.ReadLine());
+            Console.WriteLine(eleva(numero));
+            Teste(eleva(numero));
+
+
         }
+        public static void Teste(int j)
+        {
+            Console.WriteLine(j * j);
+        }
+    }
     }
 }
