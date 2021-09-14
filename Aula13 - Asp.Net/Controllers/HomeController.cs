c5e1e6767b93da2b6fe5447c19e1621b2210b6d8
Gustavo <gustavohs2004@gmail.com>
2021-09-14 11:17:27 -0300

    Aula13

---
+++ Aula13 - Asp.Net/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using Aula13___Asp.Net.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aula13___Asp.Net.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Calcula()
+        {
+            return View();
+        }
+        public IActionResult Exec1()
+        {
+            return View();
+        }
+
+        public IActionResult Exec2()
+        {
+            return View();
+        }
+
+        public IActionResult Exec3()
+        {
+            return View();
+        }
+
+        public IActionResult Exec4()
+        {
+            return View();
+        }
+
+        public IActionResult Exec5()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
